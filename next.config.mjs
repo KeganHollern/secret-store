58d63ccb1f3013f553382751f3c4f3b7cde97dc6
Kegan Hollern <keganhollern@gmail.com>
2024-12-22 17:54:37 -0600

    ssss

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone', // Enable standalone output for Docker
+    eslint: {
+        ignoreDuringBuilds: true,
+    }
 }
 
 export default nextConfig;
